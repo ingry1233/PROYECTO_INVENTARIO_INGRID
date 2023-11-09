e31d73bbd2a455f3beacc9651b04b2bf4c842849
Ingrid <iortegonqui@uniminuto.edu.co>
2023-11-09 17:27:56 -0500

    Listo primer commit

---
+++ vistas/home.php
@@ -0,0 +1,5 @@
+<div class="container is-fluid">
+	<h1 class="title">Home</h1>
+	<h2 class="subtitle">¡Bienvenido <?php echo $_SESSION['nombre']." ".$_SESSION['apellido']; ?>!</h2>
+	<img width="100%" src="https://www.microtech.es/hubfs/Fotos%20blog/inventario.jpg">
+</div>
